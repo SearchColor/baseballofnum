53d7a246984f0ab4e94cad9fc7daef5d8e185000
searchcolor <qkrdydwo1022@gmail.com>
2024-10-21 12:51:22 +0900

    feat: Add method
    입력받은 숫자 자리수 확인 메서드

--- src/lv1/App.java
+++ src/lv1/App.java
@@ -1,4 +1,10 @@
 package lv1;
 
 public class App {
+    public static void main(String[] args) {
+        DefaultSet defaultSet = new DefaultSet();
+        defaultSet.setDigit(3);
+
+        System.out.println(defaultSet.getDigit());
+    }
 }
--- src/lv1/DefaultSet.java
+++ src/lv1/DefaultSet.java
@@ -6,4 +6,8 @@ public class DefaultSet { // 게임 시작시 숫자 자리수를 설정하는 c
     public void setDigit(int n){ // 숫자 자리수 설정 메서드
         digit = n;
     }
+
+    public int getDigit() { //설정된 자리수 확인메서드
+        return digit;
+    }
 }
