ef074f1034b94807af643dd71c3e541222ebd548
searchcolor <qkrdydwo1022@gmail.com>
2024-10-21 14:44:39 +0900

    feat: Add method
    입력받은 자리수 만큼 numberset 에 랜덤숫자 추가 메서드

--- src/lv1/App.java
+++ src/lv1/App.java
@@ -4,6 +4,7 @@ public class App {
     public static void main(String[] args) {
         DefaultSet defaultSet = new DefaultSet();
         defaultSet.setDigit(3);
+        defaultSet.setNumber();
 
         System.out.println(defaultSet.getDigit());
     }
--- src/lv1/DefaultSet.java
+++ src/lv1/DefaultSet.java
@@ -1,7 +1,15 @@
 package lv1;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class DefaultSet { // 게임 시작시 숫자 자리수를 설정하는 class
     private int digit =0;
+    Random random =new Random();
+    Set<Integer> numberset = new HashSet<Integer>();
+    private int randomNum = 0;
+
 
     public void setDigit(int n){ // 숫자 자리수 설정 메서드
         digit = n;
@@ -10,4 +18,13 @@ public class DefaultSet { // 게임 시작시 숫자 자리수를 설정하는 c
     public int getDigit() { //설정된 자리수 확인메서드
         return digit;
     }
+
+    public void setNumber(){ //랜덤 수 set add
+        while(numberset.size()!=digit){
+            randomNum = (int) (Math.random() * 9)+1;
+            numberset.add(randomNum);
+        }
+    }
+
+
 }
